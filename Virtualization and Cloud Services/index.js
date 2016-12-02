21ed27af3e1ae4ed63952de27dc87ff2fd26a9e7
bdruz_000 <bdruz_000@DENESHOME>
2016-12-02 19:24:19 +0200

    Fix Virtualizaton

--- Virtualization and Cloud Services/index.js
+++ Virtualization and Cloud Services/index.js
@@ -8,18 +8,20 @@ var VerticalLine = document.querySelector('#VerticalLine'),
 
 var tl = new TimelineMax({paused:true})
 
-tl.staggerTo([RightSnake,MiddleSnake,LeftSnake],0.33, {strokeDashoffset:'-36px'})
+tl.set([RightSnake,MiddleSnake,LeftSnake],{strokeDashoffset:'72px'})
+  .staggerTo([RightSnake,MiddleSnake,LeftSnake],0.33, {strokeDashoffset:'36px'})
+  .set(Horizontal2,{strokeDashoffset:'54px'})
   .addLabel("LineDisapear")
   .to(Horizontal1,0.16,{strokeDashoffset:'27px'},'LineDisapear')
-  .to(Horizontal2,0.16,{strokeDashoffset:'-27px'},'LineDisapear')
+  .to(Horizontal2,0.16,{strokeDashoffset:'27px'},'LineDisapear')
   .to(VerticalLine,0.33,{strokeDashoffset:'15px'},'LineDisapear')
   .to(MainSnake,0.33,{strokeDashoffset:'47px'})
   .to(MainSnake,0.99,{delay:0.5,strokeDashoffset:'94px'})
   .addLabel("LineApear")
   .to(Horizontal1,0.48,{delay:0.45,strokeDashoffset:'0'},'LineApear')
-  .to(Horizontal2,0.48,{delay:0.45,strokeDashoffset:'0'},'LineApear')
+  .to(Horizontal2,0.48,{delay:0.45,strokeDashoffset:'54px'},'LineApear')
   .to(VerticalLine,0.99,{strokeDashoffset:'0'},'LineApear')
-  .staggerTo([RightSnake,MiddleSnake,LeftSnake],0.99, {strokeDashoffset:'-72px'})
+  .staggerTo([RightSnake,MiddleSnake,LeftSnake],0.99, {strokeDashoffset:'0'})
 
 var svg = document.querySelector('svg'); 
 var mouseevent =  function (e){
